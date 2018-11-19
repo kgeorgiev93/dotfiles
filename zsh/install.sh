b12e49d7497902d0badf7a78204849bfacd2e679
Krasimir Georgiev <kgeorgiev1993@gmail.com>
2018-11-19 23:41:33 +0000

    add zsh install script, update zshrc

---
+++ zsh/install.sh
@@ -0,0 +1,13 @@
+# Install zsh
+sudo apt install zsh;
+
+# Install oh-my-zsh
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh;
+
+# Change default shell
+chsh -s /bin/zsh;
+
+# Install powerlevel9k
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+# Install zsh-syntax-highlighting
