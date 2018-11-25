bb82c1ba62986b292679057d9b713810618b0af9
Krasimir Georgiev <kgeorgiev1993@gmail.com>
2018-11-25 11:51:41 +0000

    test

--- vim/vim.sh
+++ vim/vim.sh
@@ -1,13 +1,13 @@
 # Install vim
-sudo apt install vim
+sudo apt install -y vim
 
 # Install gvim
 CURRENT_DESKTOP="${XDG_CURRENT_DESKTOP}"
-if [ "$CURRENT_DESKTOP" = "KDE" ]; then
-   sudo apt install vim-gtk
-else
-   sudo apt install vim-gnome
-fi
+#if [ "$CURRENT_DESKTOP" = "KDE" ]; then
+   sudo apt install -y vim-gtk
+#else
+   #sudo apt install -y vim-gnome
+#fi
 
 # Install vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -16,16 +16,16 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
 # Set up command-t
-cd ~/.vim/bundle/command-t/ruby/command-t/ext/command-t
-ruby extconf.rb
-make
+#cd ~/.vim/bundle/command-t/ruby/command-t/ext/command-t
+#ruby extconf.rb
+#make
 
-# Set up fugitive
-cd ~/.vim/bundle
-git clone https://github.com/tpope/vim-fugitive.git
-vim -u NONE -c "helptags vim-fugitive/doc" -c q
+## Set up fugitive
+#cd ~/.vim/bundle
+#git clone https://github.com/tpope/vim-fugitive.git
+#vim -u NONE -c "helptags vim-fugitive/doc" -c q
 
-# Set up obsession
-cd ~/.vim/bundle
-git clone git://github.com/tpope/vim-obsession.git
-vim -u NONE -c "helptags vim-obsession/doc" -c q
+## Set up obsession
+#cd ~/.vim/bundle
+#git clone git://github.com/tpope/vim-obsession.git
+#vim -u NONE -c "helptags vim-obsession/doc" -c q
