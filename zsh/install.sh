2f7ef29aa374f1c9910b6afc133eb3c49aef93c6
Krasimir Georgiev <kgeorgiev1993@gmail.com>
2018-11-27 21:48:09 +0000

    install fonts-powerline from source

--- zsh/install.sh
+++ zsh/install.sh
@@ -10,8 +10,12 @@ chsh -s /bin/zsh
 # Install powerlevel9k
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
-# Install fonts
-sudo apt install -y fonts-powerline
+# Install fonts-powerline
+git clone https://github.com/powerline/fonts.git ~/fonts-powerline
+cd ~/fonts-powerline
+./install.sh
+cd ..
+rm -rf fonts-powerline
 
 # Install zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
