58f42e0dfa1c76218dc88cf9b0f02934b8608144
Krasimir Georgiev <kgeorgiev1993@gmail.com>
2018-11-27 23:12:47 +0000

    fix comment

--- zsh/install.sh
+++ zsh/install.sh
@@ -14,14 +14,14 @@ git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/theme
 
 # Install fonts-powerline
 if [[ `lsb_release -rs` == "14.04" ]]; then
-   git clone https://github.com/powerline/fonts.git ~/fonts-powerline
-   ~/fonts-powerline/install.sh
-   rm -rf ~/fonts-powerline
+   git clone https://github.com/powerline/fonts.git ~/fonts
+   ~/fonts/install.sh
+   rm -rf ~/fonts
 else
-   sudo apt install fonts-powerline
+   sudo apt install -y fonts-powerline
 fi
 
- Install zsh-syntax-highlighting
+# Install zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 # Set up zshrc
