6cb74b83e6ae39fed7456c54343e1fd906b50184
Krasimir Georgiev <kgeorgiev1993@gmail.com>
2019-07-05 16:11:31 +0100

    zsh installation using git

--- zsh/install.sh
+++ zsh/install.sh
@@ -10,13 +10,13 @@ git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 chsh -s /bin/zsh
 
 # Install powerlevel9k
-sudo apt install -y zsh-theme-powerlevel9k
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
 # Install fonts-powerline
 sudo apt install -y fonts-powerline
 
 # Install zsh-syntax-highlighting
-sudo apt install -y zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 # Install zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
