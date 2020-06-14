60f61909d2ceba3af53a0842e8e165e2a9719b3a
Krasimir Georgiev <kgeorgiev1993@gmail.com>
2020-06-15 00:55:14 +0100

    fix git checkout path

--- git/clone_all.sh
+++ git/clone_all.sh
@@ -4,8 +4,8 @@ cd ~/workspace/git/kgeorgiev93
 curl -s https://api.github.com/users/kgeorgiev93/repos | grep \"clone_url\" | awk '{print $2}' | sed -e 's/"//g' -e 's/,//g' | xargs -n1 git clone
 
 # hunter
-git clone https://github.com/cpp-pm/hunter ~/workspace/git/cpp-pm/
-git clone https://github.com/cpp-pm/hunter-testing ~/workspace/git/cpp-pm/
+git clone https://github.com/cpp-pm/hunter ~/workspace/git/cpp-pm/hunter
+git clone https://github.com/cpp-pm/hunter-testing ~/workspace/git/cpp-pm/hunter-testing
 git clone https://github.com/ruslo/polly ~/workspace/git/ruslo/polly/
 
 # grpc
