8038b6aa05611a6da1cf31ca7039afabc3a627e7
Krasimir Georgiev <kgeorgiev1993@gmail.com>
2021-10-24 18:52:09 +0100

    todos

--- git/clone_all.sh
+++ git/clone_all.sh
@@ -1,4 +1,5 @@
 # clone all my repos
+# TODO - private repos and ssh
 mkdir -p ~/workspace/git/kgeorgiev93/
 cd ~/workspace/git/kgeorgiev93
 curl -s https://api.github.com/users/kgeorgiev93/repos | grep \"clone_url\" | awk '{print $2}' | sed -e 's/"//g' -e 's/,//g' | xargs -n1 git clone
